4d4b0ac27b387f42d4b5f56d20a9a3cf43e89077
Ian <chess123mate@yahoo.ca>
2024-12-14 22:24:19 -0500

    Simplify types, add missing function definition

--- ecs.d.ts
+++ ecs.d.ts
@@ -6,43 +6,39 @@ export const Config: {
 
 /** Represents a unique set of components */
 export type Archetype = {
-	HasComponent: Set<AnyEntity>
+	HasComponent: Set<Entity>
 }
-export type AnyEntity = {
+export type Entity = {
 	/** The debug name associated with this entity */
 	Name?: string
 }
-export type Entity = Reconstruct<AnyEntity & { __entity: true }>
-export type Component<Data> = Reconstruct<
-	AnyEntity & {
-		__data: Data
-		OnAdd?: (e: AnyEntity, value: Data) => void
-		OnChange?: (e: AnyEntity, value: Data, prev: Data) => void
-		OnRemove?: (e: AnyEntity, prev: Data) => void
-		OnDelete?: (e: AnyEntity, prev: Data) => void
-	}
->
-export type Flag = Reconstruct<
-	AnyEntity & {
-		IsFlag: true
-		OnAdd?: (e: AnyEntity) => void
-		OnRemove?: (e: AnyEntity) => void
-		OnDelete?: (e: AnyEntity) => void
-	}
->
+type ComponentHooks<Data> = {
+	OnAdd?: (e: Entity, value: Data) => void
+	OnChange?: (e: Entity, value: Data, prev: Data) => void
+	OnRemove?: (e: Entity, prev: Data) => void
+	OnDelete?: (e: Entity, prev: Data) => void
+}
+export type Component<Data> = Reconstruct<Entity & { __data: Data }> & ComponentHooks<Data>
+
+type FlagHooks = {
+	OnAdd?: (e: Entity) => void
+	OnRemove?: (e: Entity) => void
+	OnDelete?: (e: Entity) => void
+}
+export type Flag = Reconstruct<Entity & { IsFlag: true }> & FlagHooks
 
 type Iter<T extends unknown[]> = IterableFunction<LuaTuple<[Entity, ...T]>>
 export type Query<T extends unknown[]> = Iter<T> & {
 	/** Only iterate over entities that have the specified components */
-	With(...components: AnyEntity[]): Query<T>
+	With(...components: Entity[]): Query<T>
 	/** Only iterate over entities that do *not* have the specified components */
-	Without(...components: AnyEntity[]): Query<T>
+	Without(...components: Entity[]): Query<T>
 	/** Only iterate over entities in archetypes for which `keep(archetype)` returns true */
 	Custom(keep: (archetype: Archetype) => boolean): Query<T>
 }
 
 type InferComponentValue<E> = E extends Component<infer T> ? T : undefined
-type InferComponentValues<A extends AnyEntity[]> = {
+type InferComponentValues<A extends Entity[]> = {
 	[K in keyof A]: InferComponentValue<A[K]>
 }
 
@@ -58,23 +54,23 @@ export class World {
 	Component<Data>(name?: string): Component<Data>
 	Flag(name?: string): Flag
 
-	Add(e: AnyEntity, C: Flag): void
-	Has(e: AnyEntity, C: AnyEntity): boolean
+	Add(e: Entity, C: Flag): void
+	Has(e: Entity, C: Entity): boolean
 	/** Combination of World:Add and associating a value between the entity and component. (Note that an entity can have a component even if its value is undefined, so `value = undefined` is valid.) */
-	Set<Data>(e: AnyEntity, C: Component<Data>, value: Data): void
+	Set<Data>(e: Entity, C: Component<Data>, value: Data): void
 	/** You can also get the data directly via e[C], so long as you treat it as read-only. */
-	Get<Data>(e: AnyEntity, C: Component<Data>): Data | undefined
-	Remove(e: AnyEntity, C: Flag): void
-	Remove<Data>(e: AnyEntity, C: Component<Data>): void
+	Get<Data>(e: Entity, C: Component<Data>): Data | undefined
+	Remove(e: Entity, C: Flag): void
+	Remove<Data>(e: Entity, C: Component<Data>): void
 	/** Removes 'e' from the world, deleting all information off of 'e' and treating it like a component and removing all information associated with it from all entities. */
-	Delete(e: AnyEntity): void
+	Delete(e: Entity): void
 	/** True if `e` is an undeleted entity. */
-	IsLiveEntity(e: unknown): e is AnyEntity
+	IsLiveEntity(e: unknown): e is Entity
 
 	/** Iterate over all entities that have the specified components.\
 	 * Note: you may change the entity under iteration in any way you wish, but changing *other* entities results in undefined behaviour.
 	 */
-	Query<T extends AnyEntity[]>(...components: T): Query<InferComponentValues<T>>
+	Query<T extends Entity[]>(...components: T): Query<InferComponentValues<T>>
 
 	/** Remove empty archetypes (good for memory and query performance, but at the cost of having to create them again later, if needed).\
 	 * Not needed if Config.AutoDeleteEmptyArchetypes is true.
@@ -83,22 +79,28 @@ export class World {
 
 	/** Extends any OnAdd behaviour defined for `C`.\
 	 * Triggered when C is added to an entity. */
-	OnAdd(C: Flag, onAdd: (e: AnyEntity) => void): void
-	OnAdd<Data>(C: Component<Data>, onAdd: (e: AnyEntity, value: Data) => void): void
+	OnAdd(C: Flag, onAdd: (e: Entity) => void): void
+	OnAdd<Data>(C: Component<Data>, onAdd: (e: Entity, value: Data) => void): void
+
 	/** Extends any OnChange behaviour defined for `C`.\
 	 * Triggered whenever the value associated with C is changed on some entity.
 	 * @param onChange `value` and `prev` are guaranteed to be different. Either could be `nil`, as OnChange will trigger along with OnAdd and OnRemove. */
-	OnChange<Data>(C: Component<Data>, onChange: (e: AnyEntity, value: Data, prev: Data) => void): void
+	OnChange<Data>(C: Component<Data>, onChange: (e: Entity, value: Data, prev: Data) => void): void
+
 	/** Extends any OnRemove behaviour defined for `C`.\
 	 * Triggered when C is removed from an entity. */
-	OnRemove(C: Flag, onRemove: (e: AnyEntity) => void): void
-	OnRemove<Data>(C: Component<Data>, onRemove: (e: AnyEntity, prev: Data) => void): void
+	OnRemove(C: Flag, onRemove: (e: Entity) => void): void
+	OnRemove<Data>(C: Component<Data>, onRemove: (e: Entity, prev: Data) => void): void
+
 	/** Extends any OnDelete behaviour defined for `C`.\
 	 * OnDelete is triggered after OnRemove if the OnRemove was triggered by world:Delete */
-	OnDelete(C: Flag, onDelete: (e: AnyEntity) => void): void
+	OnDelete(C: Flag, onDelete: (e: Entity) => void): void
 	/** Extends any OnDelete behaviour defined for `C`.\
 	 * Triggered after OnRemove if the OnRemove was triggered by world:Delete
 	 * @param onDelete `prev` refers to the value of e[C] *before* the world:Delete call
 	 */
-	OnDelete<Data>(C: Component<Data>, onDelete: (e: AnyEntity, prev: Data) => void): void
+	OnDelete<Data>(C: Component<Data>, onDelete: (e: Entity, prev: Data) => void): void
 }
+
+/** Returns true if it's an entity/component/flag that hasn't been deleted */
+export function IsLiveEntity(e: unknown): e is Entity
